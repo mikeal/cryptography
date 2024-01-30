fb0e3f596304c143caec60740278839a9eadf133
Mikeal Rogers <mikeal.rogers@gmail.com>
2024-01-30 12:54:51 -0800

    style fixes

--- scripts/components.js
+++ scripts/components.js
@@ -54,4 +54,100 @@ class CodeHighlighter extends HTMLElement {
 }
 customElements.define('code-highlighter', CodeHighlighter);
 
-export { CodeHighlighter, BN }
+class SlideshowButton extends HTMLElement {
+    connectedCallback() {
+        // Create the button
+        const button = document.createElement('button');
+        button.textContent = 'Start Slideshow';
+        button.style.position = 'absolute';
+        button.style.top = '10px';
+        button.style.right = '10px';
+
+        // Add the event listener
+        button.addEventListener('click', () => {
+            this.slideshowStart();
+        });
+
+        // Listen for the arrow keys and "esc" key
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowRight') {
+                this.slideRight();
+            } else if (event.key === 'ArrowLeft') {
+                this.slideLeft();
+            } else if (event.key === 'Escape') {
+                this.slideshowStop();
+            }
+        });
+
+        // Append the button to the shadow root
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(button);
+    }
+
+    getCards() {
+        return Array.from(document.getElementsByTagName('card'));
+    }
+
+    getCurrentCard() {
+        return this.getCards().find(card => card.classList.contains('fullscreen'));
+    }
+
+    slideshowStart() {
+        // Hide the button
+        this.shadowRoot.querySelector('button').classList.add('hidden');
+
+        // Get all card elements
+        var cards = this.getCards();
+
+        // If there are any cards, set the first one to fullscreen
+        if (cards.length > 0) {
+            cards[0].classList.add('fullscreen');
+        }
+    }
+
+    slideshowStop() {
+        // Show the button
+        this.shadowRoot.querySelector('button').classList.remove('hidden');
+
+        // Get all card elements
+        var cards = this.getCards();
+
+        // Remove the fullscreen class from all cards
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].classList.remove('fullscreen');
+        }
+    }
+
+    slideRight() {
+        let currentCard = this.getCurrentCard();
+
+        if (currentCard) {
+            // Remove the fullscreen class from the current card
+            currentCard.classList.remove('fullscreen');
+
+            // Add the fullscreen class to the next card
+            let nextIndex = (this.getCards().indexOf(currentCard) + 1) % this.getCards().length;
+            let nextCard = this.getCards()[nextIndex];
+            nextCard.classList.add('fullscreen');
+        }
+    }
+
+    slideLeft() {
+        let currentCard = this.getCurrentCard();
+
+        if (currentCard) {
+            // Remove the fullscreen class from the current card
+            currentCard.classList.remove('fullscreen');
+
+            // Add the fullscreen class to the previous card
+            let prevIndex = (this.getCards().indexOf(currentCard) - 1 + this.getCards().length) % this.getCards().length;
+            let prevCard = this.getCards()[prevIndex];
+            prevCard.classList.add('fullscreen');
+        }
+    }
+}
+
+// Register the new element
+customElements.define('slideshow-button', SlideshowButton);
+
+export { CodeHighlighter, BN, SlideshowButton }
